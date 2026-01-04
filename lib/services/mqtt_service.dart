3d1b9182f061a24f06ff6e4bf5f7325958713cd7
Oliver Zenger <oliver@zenger.org>
2026-01-04 11:20:08 +0100

    feat: add MQTT reconnection logic and proper disposal

--- lib/services/mqtt_service.dart
+++ lib/services/mqtt_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/foundation.dart';
@@ -13,6 +14,11 @@ class MqttService {
   
   Function(SoilData)? onDataReceived;
   Function(bool)? onConnectionChanged;
+  
+  Timer? _reconnectTimer;
+  int _reconnectAttempts = 0;
+  static const int _maxReconnectAttempts = 5;
+  bool _isDisposed = false;
 
   Future<void> connect() async {
     // Use a random client ID to avoid collisions
@@ -44,6 +50,7 @@ class MqttService {
   void _onConnected() {
     debugPrint('Connected to MQTT broker');
     isConnected = true;
+    _reconnectAttempts = 0; // Reset reconnect attempts on successful connection
     onConnectionChanged?.call(true);
     
     client!.subscribe(MqttConfig.topic, MqttQos.atLeastOnce);
@@ -71,9 +78,37 @@ class MqttService {
     debugPrint('Disconnected from MQTT broker');
     isConnected = false;
     onConnectionChanged?.call(false);
+    
+    // Attempt automatic reconnection
+    if (!_isDisposed && _reconnectAttempts < _maxReconnectAttempts) {
+      _scheduleReconnect();
+    } else if (_reconnectAttempts >= _maxReconnectAttempts) {
+      debugPrint('Max reconnection attempts reached. Giving up.');
+    }
+  }
+  
+  void _scheduleReconnect() {
+    _reconnectTimer?.cancel();
+    
+    // Exponential backoff: 2s, 4s, 8s, 16s, 32s
+    final delaySeconds = pow(2, _reconnectAttempts + 1).toInt();
+    _reconnectAttempts++;
+    
+    debugPrint('Scheduling reconnect attempt $_reconnectAttempts in $delaySeconds seconds...');
+    
+    _reconnectTimer = Timer(Duration(seconds: delaySeconds), () {
+      if (!_isDisposed) {
+        debugPrint('Attempting reconnection...');
+        connect();
+      }
+    });
   }
 
   void disconnect() {
+    _isDisposed = true;
+    _reconnectTimer?.cancel();
+    _reconnectAttempts = 0;
     client?.disconnect();
+    isConnected = false;
   }
 }
